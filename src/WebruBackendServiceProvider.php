e7590562e934eabaf67d1f011f89f48c731a52d9
tomasrutar <tomas.rutar@zebu.cz>
2017-02-21 00:05:22 +0100

    init

---
+++ src/WebruBackendServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace TomasRutar\WebruBackend;
+
+use Illuminate\Routing\Router;
+use Illuminate\Support\ServiceProvider;
+use Route;
+
+class WebruBackendServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/views/backend', 'backend');
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+
+        $this->publishes([
+            __DIR__.'/views/backend' => resource_path('views/backend'),
+        ]);
+    }
+}
